67a8e8599c34ff13a72ea03b7b918fe5a0314429
kellermatthis <github@matthiswieneke.de>
2022-10-23 20:23:35 +0200

    'intial'

---
+++ src/App.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+	import FileUpload from './FileUpload.svelte';
+	export let title;
+</script>
+
+<main>
+	<h1>{title}</h1>
+	<FileUpload/>
+</main>
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	h1 {
+		color: #ff3e00;
+		text-transform: uppercase;
+		font-size: 2.5em;
+		font-weight: 100;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
---
+++ src/FileUpload.svelte
@@ -0,0 +1,26 @@
+<script>	
+    let files;
+    import PDFMerger from 'pdf-merger-js/browser';
+
+    var merger = new PDFMerger();
+    $: if (files) {
+
+
+        (async () => {
+            for (let i = 0; i < files.length; i++) {
+                await merger.add(files[i]);
+                
+            }
+            await merger.save(files[0].name + '_merged.pdf');
+        })();
+	}
+</script>
+
+<p>Select the PDFs you want to merge:</p>
+<input
+	bind:files
+	id="many"
+	multiple
+	type="file"
+    accept=".pdf"
+/>
